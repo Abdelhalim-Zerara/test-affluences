1a253e064535dcb27fd5076ff2fc1f7c10468693
Abdelhalim-Zerara <zerarabdelhalim@gmail.com>
2023-05-24 11:40:34 +0200

    final

--- index.js
+++ index.js
@@ -1,65 +1,73 @@
-const express = require('express');
-const moment = require('moment');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
-
 
 const PORT = 5000;
 
 app.use(express.json());
 
-
-
-app.get('/availability', async (req, res) => {
+app.get("/availability", async (req, res) => {
   const datetime = req.query.datetime;
   const resourceId = req.query.resourceId;
-  
 
   if (resourceId != 1337) {
-    res.status(404).json({ error: 'resource not found' });
+    res.status(404).json({ error: "resource not found" });
     return;
   }
 
-  const requestedDatetime = moment(datetime);
+  const requestedDatetime = new Date(datetime);
+  const date = requestedDatetime.toISOString().split("T")[0];
 
   try {
     const [reservationsResponse, timetablesResponse] = await Promise.all([
-      axios.get('http://localhost:8080/timetables?date=2020-03-19&resourceId=1337'),
-      axios.get('http://localhost:8080/timetables?date=2020-03-19&resourceId=1337'),
+      axios.get(
+        `http://localhost:8080/reservations?date=${date}&resourceId=1337`
+      ),
+      axios.get(
+        `http://localhost:8080/timetables?date=${date}&resourceId=1337`
+      ),
     ]);
 
     const reservations = reservationsResponse.data.reservations;
     const timetables = timetablesResponse.data.timetables;
 
-  const isAvailable = reservations.every((reservation) => {
-  const reservationStart = moment(reservation.reservationStart);
-  const reservationEnd = moment(reservation.reservationEnd);
-  if (requestedDatetime.isBetween(reservationStart, reservationEnd, null, '[]')) {
-    return false; 
-  }
+    if (!timetablesResponse.data.open) {
+      res.json({ available: false });
+    }
 
-  const timetable = timetables.find((timetable) => {
-    const opening = moment(timetable.opening);
-    const closing = moment(timetable.closing);
-    return requestedDatetime.isBetween(opening, closing, null, '[]');
-  });
+    if (!reservations) {
+      res.json({ available: false });
+    }
 
-  if (!timetable) {
-    return false; // Not available
-  }
+    const timetable = timetables.some((timetable) => {
+      const opening = new Date(timetable.opening);
+      const closing = new Date(timetable.closing);
+      return requestedDatetime >= opening && requestedDatetime < closing;
+    });
 
-  return true; // Available
-  });
-    res.json({ available: true });
+    const reservation = reservations.every((reservation) => {
+      const reservationStart = new Date(reservation.reservationStart);
+      const reservationEnd = new Date(reservation.reservationEnd);
+
+      if (
+        requestedDatetime >= reservationStart &&
+        requestedDatetime < reservationEnd
+      ) {
+        return false;
+      }
+      return true;
+    });
+
+    const isAvailable = reservation && timetable;
+
+    res.json({ available: isAvailable });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-
-app.listen(PORT, (req, res) => {
-    console.log('listening on port ' + PORT);
-})
-
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
